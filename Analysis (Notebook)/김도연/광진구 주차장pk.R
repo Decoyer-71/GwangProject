4236f813250244d62bca26b0b2beb957f6860ded
kim <doyeon7507@gmail.com>
2023-04-14 09:29:33 +0900

    0413

---
+++ Analysis (Notebook)/김도연/광진구 주차장pk.R
@@ -0,0 +1,27 @@
+install.packages('rJava')
+install.packages('DBI')
+install.packages('RMySQL')
+
+library(RMySQL)
+
+conn <- dbConnect(MySQL(), dbname= 'testdb', user='root', '1111')
+conn
+
+result <- dbGetQuery(conn, 'select * from emp')
+result
+
+dbListFields(conn, 'emp')
+
+# DML
+dbSendQuery(conn, 'delete from tbla')
+dbGetQuery(conn, 'select * from tbla')
+
+# 파일로부터 데이터를 읽어들여 DB에 저장
+file_score<- read.csv('../data/score.csv', header=T)
+file_score                      
+
+dbWriteTable(conn, 'score', file_score, row.names=F)
+result<- dbGetQuery(conn, 'select * from score')
+result
+
+dbDisconnect(conn)
